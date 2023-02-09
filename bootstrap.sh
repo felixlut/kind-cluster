7bb47865a626005f4f87319a8aab04d9411272f9
Felix Luthman <felix.luthman@gmail.com>
2023-02-09 14:15:17 +0100

    bootstrap local kind cluster

---
+++ bootstrap.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Vars
+export CLUSTER_NAME="bootstrap-cluster"
+
+# Create cluster
+kind create cluster --name ${CLUSTER_NAME}
+# kind create cluster --config kind-settings/cluster.yml --name ${CLUSTER_NAME}
+
+# Preload images
+# https://kubernetes.io/docs/tasks/access-application-cluster/list-all-running-container-images/#list-all-container-images-in-all-namespaces
+# kubectl get pods --all-namespaces -o jsonpath="{.items[*].spec.containers[*].image}" |\
+# tr -s '[[:space:]]' '\n' |\
+# sort |\
+# uniq > kind-settings/preloaded-images.txt 
+cat kind-settings/preloaded-images.txt | xargs -I{} sh -c 'docker pull {} && kind load docker-image {} {} --name ${CLUSTER_NAME}'
+
+# Apply infra resources (argocd, ...)
+kubectl apply -k config/infra
+
+
+# Do something similar for argocd?!? 
+# Nginx ingress controller pre-check
+# kubectl wait --namespace ingress-nginx \
+#  --for=condition=ready pod \
+ # --selector=app.kubernetes.io/component=controller \
+  #--timeout=300s
+
+
+
+# Cleanup
+# kind delete cluster --name ${CLUSTER_NAME}
